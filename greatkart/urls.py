c1ef733666922a892be06020c9074b9a7c5920bf
Linh Bui <126184651+Linhbuikhanh@users.noreply.github.com>
2025-05-22 08:26:48 +0700

    setup rosetta

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -18,15 +18,24 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
+# URL cho xử lý chuyển ngôn ngữ (bắt buộc giữ bên ngoài)
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+# Các URL chính nằm trong i18n_patterns để hoạt động với /ja/, /en/
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
     path('rosetta/', include('rosetta.urls')),
-
-    # ORDERS
     path('orders/', include('orders.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+# Phục vụ file media
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
